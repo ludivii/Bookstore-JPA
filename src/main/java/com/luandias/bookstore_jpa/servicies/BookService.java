31c213301a1760ad03d9cdfa4bb0d6ba42495e46
Luan <luandsvt@gmail.com>
2024-12-04 08:44:44 -0300

    Creation of BookService

---
+++ src/main/java/com/luandias/bookstore_jpa/servicies/BookService.java
@@ -0,0 +1,43 @@
+package com.luandias.bookstore_jpa.servicies;
+
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luandias.bookstore_jpa.dtos.BookRecordDTO;
+import com.luandias.bookstore_jpa.models.BookModel;
+import com.luandias.bookstore_jpa.models.ReviewModel;
+import com.luandias.bookstore_jpa.repositories.AuthorRepository;
+import com.luandias.bookstore_jpa.repositories.BookRepository;
+import com.luandias.bookstore_jpa.repositories.PublisherRepository;
+
+@Service
+public class BookService {
+
+	private final BookRepository bookRepository;
+	private final AuthorRepository authorRepository;
+	private final PublisherRepository publisherRepository;
+
+	public BookService(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+		this.bookRepository = bookRepository;
+		this.authorRepository = authorRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+	@Transactional
+	public BookModel saveBook(BookRecordDTO bookRecordDTO) {
+		BookModel book = new BookModel();
+		book.setTitle(bookRecordDTO.title());
+		book.setPublisher(publisherRepository.findById(bookRecordDTO.publisherId()).get());
+		book.setAuthors(authorRepository.findAllById(bookRecordDTO.authorIds()).stream().collect(Collectors.toSet()));
+
+		ReviewModel reviewModel = new ReviewModel();
+		reviewModel.setComment(bookRecordDTO.reviewComment());
+		reviewModel.setBook(book);
+		book.setReview(reviewModel);
+
+		return bookRepository.save(book);
+	}
+
+}
