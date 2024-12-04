9bbf59d89b250da93cdf4b4432a91fe9ba1475bb
Luan <luandsvt@gmail.com>
2024-12-04 07:01:53 -0300

    Project started

---
+++ src/main/java/com/luandias/bookstore_jpa/BookstoreJpaApplication.java
@@ -0,0 +1,13 @@
+package com.luandias.bookstore_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luandias/bookstore_jpa/BookstoreJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandias.bookstore_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
